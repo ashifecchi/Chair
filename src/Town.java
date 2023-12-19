370a310dc41dad0bd3beaab78300e8c30d44096a
BT_1N3_31 <@>
2023-12-19 10:33:59 -0500

    completed TH-4

--- src/Town.java
+++ src/Town.java
@@ -124,16 +124,18 @@ public class Town {
      */
     private Terrain getNewTerrain() {
         double rnd = Math.random();
-        if (rnd < .2) {
+        if (rnd < .1) {
             return new Terrain("Mountains", "Rope");
-        } else if (rnd < .4) {
+        } else if (rnd < .2) {
             return new Terrain("Ocean", "Boat");
-        } else if (rnd < .6) {
+        } else if (rnd < .4) {
             return new Terrain("Plains", "Horse");
-        } else if (rnd < .8) {
+        } else if (rnd < .6) {
             return new Terrain("Desert", "Water");
-        } else {
+        } else if (rnd < .8){
             return new Terrain("Jungle", "Machete");
+        } else {
+            return new Terrain("Marsh", "Boots");
         }
     }
 
